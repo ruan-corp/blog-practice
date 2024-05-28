cd72a822adb55b325dd27d6efa0c62a422a32731
Ruan Rigon Alves <ruanrigon2k@gmail.com>
2024-05-28 15:34:26 -0300

    testing

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,18 @@
 <?php
 
-use Illuminate\Routing\RouteGroup;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('web/sections/home/home');
+Route::middleware('auth')->prefix('admin')->group(function () {
+    Route::get('/', [HomeController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
+
+    Route::prefix('profile')->group(function () {
+        Route::get('/', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::patch('/', [ProfileController::class, 'update'])->name('profile.update');
+        Route::delete('/', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    });
 });
+
+
+require __DIR__ . '/auth.php';
